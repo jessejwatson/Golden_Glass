e72b1600b98798d92b86eb408b444e462ff5d6b8
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2022-12-03 10:51:53 +1100

    Added rel="noreferrer"

--- components/footerIcons.tsx
+++ components/footerIcons.tsx
@@ -12,22 +12,22 @@ export default function FooterIcons() {
       
       <div className='flex justify-center pb-6'>
         <div className='px-4'>
-          <a href="https://www.youtube.com/@Jessals04" target="_blank">
+          <a href="https://www.youtube.com/@Jessals04" target="_blank" rel="noreferrer">
             <SiYoutube color='#5AC494' fontSize={24}/>
           </a>
         </div>
         <div className='px-4'>
-          <a href="https://twitter.com/jessals04" target="_blank">
+          <a href="https://twitter.com/jessals04" target="_blank" rel="noreferrer">
             <SiTwitter color='#5AC494' fontSize={24}/>
           </a>
         </div>
         <div className='px-4'>
-          <a href="https://open.spotify.com/user/yutzwhvn77ybml43prhsbzd8d?si=b48a448326f5469f" target="_blank">
+          <a href="https://open.spotify.com/user/yutzwhvn77ybml43prhsbzd8d?si=b48a448326f5469f" target="_blank" rel="noreferrer">
             <SiSpotify color='#5AC494' fontSize={24}/>
           </a>
         </div>
         <div className='px-4'>
-          <a href="https://github.com/Jessals04/" target="_blank">
+          <a href="https://github.com/Jessals04/" target="_blank" rel="noreferrer">
             <SiGithub color='#5AC494' fontSize={24}/>
           </a>
         </div>
