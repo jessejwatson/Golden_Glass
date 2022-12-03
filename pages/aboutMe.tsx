c9000a6a2952c5ef31f0fe594a242ec4d39f81a3
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2022-12-03 11:20:03 +1100

    Create aboutMe.tsx

---
+++ pages/aboutMe.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+// Components
+import NavBar from '../components/navBar'
+
+export default function Body() {
+  return (
+    <div className='px-16 flex justify-center content-center'>
+        
+    </div>
+  )
+}
