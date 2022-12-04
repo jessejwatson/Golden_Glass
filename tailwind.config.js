cfd9a38bab5dc895f985a1268d86c72598288076
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2022-12-05 09:21:09 +1100

    Created fade-in animations

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,7 +15,33 @@ module.exports = {
     extend: {
       fontFamily: {
         FiraCode: ["Fira Code"],
+      },
+      keyframes: {
+        'fade-in-down': {
+            '0%': {
+                opacity: '0',
+                transform: 'translateY(-20px)'
+            },
+            '100%': {
+                opacity: '1',
+                transform: 'translateY(0)'
+            },
+        },
+        'fade-in-up': {
+          '0%': {
+              opacity: '0',
+              transform: 'translateY(20px)'
+          },
+          '100%': {
+              opacity: '1',
+              transform: 'translateY(0)'
+          },
       }
+    },
+    animation: {
+        'fade-in-down': 'fade-in-down 0.5s ease-out',
+        'fade-in-up': 'fade-in-up 0.5s ease-out'
+    }
     },
   },
   plugins: [],
