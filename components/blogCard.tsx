6241dabf8f0a0e7521e86d6970423240fca89b9a
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2022-12-06 10:36:14 +1100

    INCOMPLETE: creating data requests

---
+++ components/blogCard.tsx
@@ -0,0 +1,9 @@
+import Link from 'next/link'
+
+export default function BlogCard() {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- pages/blogList.tsx
+++ pages/blogList.tsx
@@ -1,12 +1,54 @@
 import ComingSoon from "../components/comingSoon";
+import { GraphQLClient, gql } from 'graphql-request'
+import BlogCard from "../components/BlogCard";
 
-export default function BlogList() {
+const graphcms = new GraphQLClient(
+  'https://api-ap-southeast-2.hygraph.com/v2/clazxnzw1231r01uhc0ke79zu/master'
+  );
+
+const QUERY = gql`
+  {
+    BlogPost{
+      id,
+      title,
+      publishDate,
+      slug,
+      content {
+        html
+      }
+      author {
+        name,
+        avatar {
+          url
+        }
+      }
+      coverImage {
+        url
+      }
+    }
+  }
+`
+
+export async function getStaticProps() {
+  const { posts } = await graphcms.request(QUERY);
+
+  return {
+    props: {
+      posts,
+    },
+    revalidate: 10,
+  };
+}
+
+export default function BlogList({posts}) {
     return (
       <div className='h-screen'>
         <div className='font-FiraCode bg-gradient-to-br from-c-charcoal to-c-blue text-c-white h-full'>
-          <div className="p-20">
-            <ComingSoon/>
-          </div>
+          <main>
+            {posts.map((post) => (
+              <BlogCard title={post.title}}/>
+            ))}
+          </main>
         </div>
       </div>
     )
