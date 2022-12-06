53c40e1db1b18c87c08b622a4f6af31c28a468b9
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2022-12-06 21:32:24 +1100

    Still styling inner blog

--- components/blogCard.tsx
+++ components/blogCard.tsx
@@ -4,16 +4,16 @@ import { AiFillRightCircle } from 'react-icons/ai'
 
 export default function BlogCard({id, title, publishDate, slug, contentHTML, authorName, avatarURL, coverImageURL}) {
   return (
-    <div className='mx-16 p-3 bg-c-charcoal rounded-lg animate-fade-in-up'>
+    <div>
         <Link href={'/posts/' + slug}>
-            <div className='flex justify-between gap-x-10'>
-                <div>
-                    <h1 className='text-xl'>{title}</h1>
+            <div className='p-3 bg-c-charcoal bg-opacity-75 rounded-lg animate-fade-in-up max-w-3xl flex justify-between gap-x-10 hover:translate-y-2 active:opacity-90'>
+                <div className='text-xl w-full'>
+                    <h1>{title}</h1>
                 </div>
-                <div className='w-1/12 h-full hidden md:block'>
+                <div className='w-3/12 my-auto hidden md:block'>
                     <img src={coverImageURL} alt="Cover Image" />
                 </div>
-                <div className=' my-auto'>
+                <div className='my-auto md:pr-6'>
                     <AiFillRightCircle size={30} color='#D3D4D9'/>
                 </div>
             </div>
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,7 +1,9 @@
 import { GraphQLClient, gql } from 'graphql-request'
 import NavBar from '../../components/navBar';
-import styles from "../../styles/Slug.module.css";
-import Image from 'next/image'
+import { AiOutlineArrowLeft, AiOutlineShareAlt, AiOutlineLike } from 'react-icons/ai'
+import Link from 'next/link';
+import FooterIcons from '../../components/footerIcons'
+import styles from '../../styles/Slug.module.css'
 
 const graphcms = new GraphQLClient(
     'https://api-ap-southeast-2.hygraph.com/v2/clazxnzw1231r01uhc0ke79zu/master'
@@ -64,14 +66,46 @@ export async function getStaticProps({ params }) {
 
 export default function BlogPost({ post }) {
     return (
-        <main className={styles.Slug}>
-            <div>
-                <div>
+        <main className='h-full'>
+            <div className='font-FiraCode bg-gradient-to-br from-c-charcoal to-c-blue text-c-white h-full grid grid-flow-row'>
+                <section className='hidden md:block pb-8'>
                     <NavBar/>
-                </div>
-                <div>
-                    <img className=' w-2/3 h-80' src={post.coverImage.url}/>
-                </div>
+                </section>
+
+                {/* Cover */}
+                <section>
+                    <div className='flex justify-center'>
+                        <img className='coverImg p-2 object-cover w-full h-80 max-w-6xl' src={post.coverImage.url}/>
+                    </div>
+
+                    <div className='px-4 -mt-7 max-w-6xl flex justify-between'>
+                        <Link href={'/blogList'}>
+                            <div className='p-1.5 bg-c-white rounded-full w-fit hover:translate-y-1 active:opacity-90'><AiOutlineArrowLeft color='#221D23' size={25}/></div>
+                        </Link>
+                        <div className='flex gap-x-4'>
+                            <Link href={''}>
+                                <div className='p-1.5 bg-c-white rounded-full w-fit hover:translate-y-1 active:opacity-90'><AiOutlineShareAlt color='#221D23' size={25}/></div>
+                            </Link>
+                            <Link href={''}>
+                                <div className='p-1.5 bg-c-white rounded-full w-fit hover:translate-y-1 active:opacity-90'><AiOutlineLike color='#221D23' size={25}/></div>
+                            </Link>
+                        </div>
+                    </div>
+                </section>
+
+                {/* Title and content */}
+                <section className='px-10 pt-5 pb-5 flex justify-center'>
+                    <div>
+                        <h1 className='text-xl'>{post.title}</h1>
+                        <div className='max-w-xl' dangerouslySetInnerHTML={{ __html: post.content.html }}></div>
+                    </div>
+                </section>
+
+                {/* Socials */}
+                <section className={styles.content}>
+                    <FooterIcons/>
+                </section>
+
             </div>
         </main>
     )
