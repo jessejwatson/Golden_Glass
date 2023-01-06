93443b20386db6d040352b39f76c4f86399be248
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2023-01-06 14:34:25 +1100

    Initial Projects Commit

---
+++ components/thisWebsite.tsx
@@ -0,0 +1,13 @@
+import Image from 'next/image'
+import thisWebsiteImg from '../assets/thisWebsiteImg.png'
+
+export default function ThisWebsite() {
+    return (
+        <div className="p-4 flex flex-col gap-4 m-auto p-2rounded-lg max-w-4xl noDrop border-2 border-opacity-25 border-c-grey bg-c-grey bg-opacity-5">
+            <h1 className='text-xl font-semibold'>This Website (code name: GoldenGlass)</h1>
+            <Image className='coverImg' src={thisWebsiteImg} alt={'Profile Picture'} width={500}/>
+            <p className='max-w-lg'>I've always wanted my own website. But not only that, one that I can say I built from scratch. And that is what you are looking at right now. I built this website using ReactJS libraries and the NextJS framework.</p>
+            <a className="text-c-green hover:underline" href="/post/How-I-Built-The-MyHealth-App">Read more here &gt;</a>
+        </div>
+    )
+}
--- pages/projectsList.tsx
+++ pages/projectsList.tsx
@@ -1,11 +1,26 @@
 import ComingSoon from "../components/comingSoon";
+import FooterIcons from "../components/footerIcons";
+import MyHealth from "../components/myHealth";
+import NavBar from "../components/navBar";
+import ThisWebsite from "../components/thisWebsite";
 
 export default function ProjectsList() {
     return (
       <div className='h-screen'>
-        <div className='font-FiraCode bg-gradient-to-br from-c-charcoal to-c-blue text-c-white h-full'>
-          <div className="p-20">
-            <ComingSoon/>
+        <div className='font-FiraCode bg-gradient-to-br from-c-charcoal to-c-blue text-c-white h-full grid grid-flow-row min-h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-c-green scrollbar-track-c-blue'>
+          <div>
+            <NavBar/>
+          </div>
+
+          <section className="max-w-7xl m-auto">
+            <div className="p-6 flex flex-col gap-8 md:flex-row">
+              <MyHealth/>
+              <ThisWebsite/>
+            </div>
+          </section>
+
+          <div className='grid grid-flow-col content-end'>
+            <FooterIcons/>
           </div>
         </div>
       </div>
