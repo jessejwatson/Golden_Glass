9ee32065ee6aae5dc7b4fa802d8438cac82e3399
Jesse Watson <95267462+Jessals04@users.noreply.github.com>
2023-01-13 06:15:04 +1100

    Update thisWebsite.tsx

--- components/thisWebsite.tsx
+++ components/thisWebsite.tsx
@@ -7,7 +7,7 @@ export default function ThisWebsite() {
             <h1 className='text-xl font-semibold'>This Website (code name: GoldenGlass)</h1>
             <Image className='coverImg' src={thisWebsiteImg} alt={'Profile Picture'} width={500}/>
             <p className='max-w-lg'>I&apos;ve always wanted my own website. But not only that, one that I can say I built from scratch. And that is what you are looking at right now. I built this website using ReactJS libraries and the NextJS framework.</p>
-            <a className="text-c-green hover:underline" href="">Read more here &gt;</a>
+            <a className="text-c-green hover:underline" href="posts/how-i-built-this-nextjs-website">Read more here &gt;</a>
         </div>
     )
 }
